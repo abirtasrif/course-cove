947cf2456e24cab8971d7799285e6fb5ae55dbde
abirtasrif <abirtasrif@gmail.com>
2023-05-11 23:56:54 +0600

    singleCourseBug present

--- pages/api/courses/[courseid].js
+++ pages/api/courses/[courseid].js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   try {
     if (req.method === "GET") {
       const course = await getSingleCourse(req.query.courseId);
+
       return res.status(200).json(course);
     }
   } catch (error) {
