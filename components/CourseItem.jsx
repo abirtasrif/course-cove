b82ed607d240123c07408c188664e908825255d0
abirtasrif <abirtasrif@gmail.com>
2023-05-20 13:42:36 +0600

    optimized media and cdn added

--- components/CourseItem.jsx
+++ components/CourseItem.jsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import { CldImage } from "next-cloudinary";
 
 const CourseItem = ({ course }) => {
   return (
     <div>
-      <Image src={course.cover} alt={course.title} width={640} height={360} />
+      <CldImage
+        src={course.cover}
+        alt={course.title}
+        width={640}
+        height={360}
+        crop="fill"
+        gravity="auto"
+        sizes="50w"
+        priority
+      />
     </div>
   );
 };
