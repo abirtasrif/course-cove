7c898b400f7dd5a522f24cd4b8e16964bdd4528b
abirtasrif <abirtasrif@gmail.com>
2023-06-04 00:22:32 +0600

    added success page

--- pages/success.js
+++ pages/success.js
@@ -1,5 +1,20 @@
+import Button from "@/components/Button";
+import { AiFillCheckCircle } from "react-icons/ai";
+
 const successPage = () => {
-  return <div>successPage</div>;
+  return (
+    <div className="wrapper py-10 min-h-screen flex flex-col items-center gap-7 mt-10">
+      <div>
+        <h2 className="text-3xl flex items-center gap">
+          <span>
+            <AiFillCheckCircle className="text-green-500" />
+          </span>
+          You&apos;ve enrolled successfully
+        </h2>
+      </div>
+      <Button href="/orders" placeholder="Go to your orders" />
+    </div>
+  );
 };
 
 export default successPage;
